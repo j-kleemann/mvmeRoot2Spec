5a5f8a49fc0e100fd5995df40484a229e8650ff3
Jörn Kleemann <jkleemann@ikp.tu-darmstadt.de>
2021-05-13 09:35:38 +0200

    Progress info and uproot parallelization
    The limiting factor in speed should now be the numpy operations (filtering NaNs, calibrating, histogramming)

--- mvmeRoot2Spec.py
+++ mvmeRoot2Spec.py
@@ -28,11 +28,11 @@ import numpy as np
 import uproot
 
 def parseRawHistBinningSpecs(argStr) :
-  exception=argparse.ArgumentTypeError("Rebinning factors for raw spectra must be comma-separated positive integer numbers!")
+  exception = argparse.ArgumentTypeError("Rebinning factors for raw spectra must be comma-separated positive integer numbers!")
   try:
     if "." in argStr:
       raise exception
-    argList=[int(s) for s in argStr.split(",")]
+    argList = [int(s) for s in argStr.split(",")]
     if any(i <= 0 for i in argList):
       raise exception
     return argList
@@ -40,15 +40,15 @@ def parseRawHistBinningSpecs(argStr) :
     raise exception
 
 def parseCalHistBinningSpecs(argStr) :
-  exception=argparse.ArgumentTypeError("Binning specifications for energy calibrated spectra must be a comma-separated list of square-bracketed, comma-separated tuples of positive numbers!")
+  exception = argparse.ArgumentTypeError("Binning specifications for energy calibrated spectra must be a comma-separated list of square-bracketed, comma-separated tuples of positive numbers!")
   try:
-    match=re.fullmatch(r"\s*\[\s*(.*)\s*\]\s*", argStr)
+    match = re.fullmatch(r"\s*\[\s*(.*)\s*\]\s*", argStr)
     if not match:
       raise exception
-    argStr=match.group(1)
-    argList=re.split(r"\s*\]\s*,\s*\[\s*", argStr)
-    argList=[re.split(r"\s*,\s*", tupleStr) for tupleStr in argList]
-    argList=[[float(i) for i in tup] for tup in argList]
+    argStr = match.group(1)
+    argList = re.split(r"\s*\]\s*,\s*\[\s*", argStr)
+    argList = [re.split(r"\s*,\s*", tupleStr) for tupleStr in argList]
+    argList = [[float(i) for i in tup] for tup in argList]
     if any(len(tup)!=2 or any(i <= 0 for i in tup) for tup in argList):
       raise exception
     return argList
@@ -56,7 +56,7 @@ def parseCalHistBinningSpecs(argStr) :
     raise exception
 
 
-programName=os.path.basename(sys.argv[0])
+programName = os.path.basename(sys.argv[0])
 
 # Parse command line arguments in an nice way with automatic usage message etc
 argparser = argparse.ArgumentParser(description="Sort events of a ROOT file exported by the mvme_root_client from a Mesytech VME DAQ into spectra (histograms). Specifically aimed at the mvme DAQ used since 2021 at the High Intensity γ-ray Source (HIγS) facility, located at the Triangle Universities Nuclear Laboratory in Durham, NC, USA.")
@@ -74,67 +74,67 @@ argparser.add_argument("--noaddback", dest="exportAddback", action="store_false"
 argparser.add_argument("--outcaloverwrite", dest="outcalOverwrite", action="store_true", help="Use to overwrite a potentially existing file, when writing the output energy calibration list file. If not used, writing appends to an existing file.")
 
 
-cliArgs=argparser.parse_args()
+cliArgs = argparser.parse_args()
 rootFilePath = cliArgs.rootFilePath
 inCalFilePath = cliArgs.inCalFilePath
 outCalFilePath = cliArgs.outCalFilePath
 outDir = cliArgs.outDir
-rawHistRebinningFactors=cliArgs.rawHistRebinningFactors
-calHistBinningSpecs=cliArgs.calHistBinningSpecs
-exportRaw=cliArgs.exportRaw
-exportCal=cliArgs.exportCal
-exportAddback=cliArgs.exportAddback
-outcalOverwrite=cliArgs.outcalOverwrite
-verbose=cliArgs.verbose
+rawHistRebinningFactors = cliArgs.rawHistRebinningFactors
+calHistBinningSpecs = cliArgs.calHistBinningSpecs
+exportRaw = cliArgs.exportRaw
+exportCal = cliArgs.exportCal
+exportAddback = cliArgs.exportAddback
+outcalOverwrite = cliArgs.outcalOverwrite
+verbose = cliArgs.verbose
 
 # The given ROOT file must exist
 if not os.path.isfile(rootFilePath) :
     exit("ERROR: Supplied ROOTFILE '" + rootFilePath + "' is no valid file!")
 
-startTime=datetime.datetime.now()
+startTime = datetime.datetime.now()
 print(startTime.strftime("%Y-%m-%d %H:%M:%S"), "- Executing commandline", "'"+"' '".join(map(str, sys.argv))+"'")
 
 # Helper to get nice filename prefix from rootFilePath
 @functoolsLRUCache(maxsize=None)
 def rootFilePathToPrefix(filePath):
-  filePath=os.path.basename(filePath)
+  filePath = os.path.basename(filePath)
   if filePath.startswith("mvmelst_") :
-    filePath="run"+filePath[8:]
+    filePath = "run"+filePath[8:]
   if "." in filePath :
-    filePath=filePath.rpartition(".")[0]
+    filePath = filePath.rpartition(".")[0]
   if filePath.endswith("_raw") :
-    filePath=filePath[:-4]
+    filePath = filePath[:-4]
   return filePath
 
 # Default values for cliArgs
 if outCalFilePath is None :
   outCalFilePath = rootFilePathToPrefix(rootFilePath)+"_sorted.callst"
 if outDir is None :
-  outDir=rootFilePathToPrefix(rootFilePath)+"_sorted"
+  outDir = rootFilePathToPrefix(rootFilePath)+"_sorted"
 if rawHistRebinningFactors is None :
-  rawHistRebinningFactors=[1]
+  rawHistRebinningFactors = [1]
 if calHistBinningSpecs is None :
-  calHistBinningSpecs=[[1., 20e3]]
+  calHistBinningSpecs = [[1., 20e3]]
 
 os.makedirs(outDir, exist_ok=True)
 
 
 # Hardcoded settings
-mvmeModulesChannelsDict={
+mvmeModulesChannelsDict = {
   'clover_135/amplitude_clover_135[16]': range(16),
   'clover_90/amplitude_clover_90[16]': range(16),
   'cebr/integration_long[16]': range(16),
 }
-addbackChannelDict={
+addbackChannelDict = {
   'clover_135/amplitude_clover_135[16]': {1: [0, 1, 2, 3], 2: [4, 5, 6, 7], 3: [8, 9, 10, 11], 4: [12, 13, 14, 15]},
   'clover_90/amplitude_clover_90[16]':   {1: [0, 1, 2, 3], 2: [4, 5, 6, 7], 3: [8, 9, 10, 11], 4: [12, 13, 14, 15]},
 }
 
 # Global variables and constants
-calsDict=dict()
-hists=dict()
-digitizerBins=2**16
-calFilenameRegex=re.compile(fr"({rootFilePathToPrefix(rootFilePath)}_.+)_raw_b(\d+).txt:?")
+calsDict = dict()
+hists = dict()
+digitizerBins = 2**16
+calFilenameRegex = re.compile(fr"({rootFilePathToPrefix(rootFilePath)}_.+)_raw_b(\d+).txt:?")
 
 rawHistBinnings = [{
   "binWidth": rebinningFactor,
@@ -156,16 +156,16 @@ if inCalFilePath is not None:
   with open(inCalFilePath) as f:
     for line in f:
       specName, *valStrs = line.split()
-      regexMatch=calFilenameRegex.fullmatch(specName)
+      regexMatch = calFilenameRegex.fullmatch(specName)
       if regexMatch :
-        cal=np.polynomial.Polynomial(np.array([float(x) for x in valStrs]))
+        cal = np.polynomial.Polynomial(np.array([float(x) for x in valStrs]))
         # HDTV uses centered bins and exports the calibration matching this convention
         # The MVME DAQ ROOT exporter however randomizes the integer channel values by adding a [0,1) uniform distributed random number, i.e. assuming a integer-edge binning
         # Furthermore the raw data could have been binned with a binsize!=1 which hdtv does not know
         # To correct for this difference in scheme, create the composition of first shifting the raw channels to the hdtv
         # coresponding channel values (back by 0.5 and shrinked by 1/rawRebinningFactor used) and then applying the HDTV calibration
-        calRebinningFactor=int(regexMatch.group(2))
-        cal=cal(np.polynomial.Polynomial([-0.5, 1/calRebinningFactor]))
+        calRebinningFactor = int(regexMatch.group(2))
+        cal = cal(np.polynomial.Polynomial([-0.5, 1/calRebinningFactor]))
         calsDict[regexMatch.group(1)] = cal
 
 
@@ -177,7 +177,7 @@ def getHistFilenamePrefix(rootFilename, module, detNo) :
 
 # Helper function to sort events into histograms, accumulate histograms from blocks of data and initialize histograms
 def accumulateHist(histNamePrefix, data, histBinning, calState) :
-  histKey=histNamePrefix+"_"+calState+"_b"+str(histBinning["binWidth"])+".txt"
+  histKey = histNamePrefix+"_"+calState+"_b"+str(histBinning["binWidth"])+".txt"
   if histKey not in hists:
     hists[histKey] = {"calState": calState, "binning": histBinning, "cts": np.zeros(histBinning["nBins"])}
   hists[histKey]["cts"] += np.histogram(data, histBinning["nBins"], (histBinning["lowerEdge"], histBinning["upperEdge"]))[0]
@@ -186,43 +186,58 @@ def accumulateHist(histNamePrefix, data, histBinning, calState) :
 
 # Helper function to process a data block of a detector
 def processDataBlock(dataID, data) :
-  histNamePrefix=getHistFilenamePrefix(*dataID)
+  histNamePrefix = getHistFilenamePrefix(*dataID)
   if exportRaw:
     for rawHistBinning in rawHistBinnings:
       accumulateHist(histNamePrefix, data, rawHistBinning, "raw")
   if exportCal and histNamePrefix in calsDict :
-    data=calsDict[histNamePrefix](data)
+    data = calsDict[histNamePrefix](data)
     for calHistBinning in calHistBinnings:
       accumulateHist(histNamePrefix, data, calHistBinning, "cal")
 
 
 # Helper function to process a data block of multiple channels for addback
 def processAddbackDataBlock(dataID, channels, data) :
-  addbackData=np.zeros(data.shape[1])
+  addbackData = np.zeros(data.shape[1])
   for channelNo, channelData in zip(channels, data) :
-    calName=getHistFilenamePrefix(*dataID[:2], f"Det_{channelNo+1}")
+    calName = getHistFilenamePrefix(*dataID[:2], f"Det_{channelNo+1}")
     if calName not in calsDict :
       return
-    channelData=calsDict[calName](channelData) # To optimize performance one could try reusing the result from the processDataBlock call...
-    addbackData+=np.nan_to_num(channelData)
-  addbackData=addbackData[addbackData!=0]
-  histNamePrefix=getHistFilenamePrefix(*dataID)
+    # To optimize performance one could try reusing the calibrated data from the processDataBlock call, but beware that 
+    # at the moment the processDataBlock has its NaNs dropped and dropping NaNs early speeds up the following computations noticeably...
+    channelData = calsDict[calName](channelData)
+    addbackData += np.nan_to_num(channelData)
+  addbackData = addbackData[addbackData!=0]
+  histNamePrefix = getHistFilenamePrefix(*dataID)
   for calHistBinning in calHistBinnings:
     accumulateHist(histNamePrefix, addbackData, calHistBinning, "cal")
 
-
 # Main loop over the data
-with uproot.open(rootFilePath) as rootFile :
-  for moduleDictBlock in rootFile['event0'].iterate(tuple(mvmeModulesChannelsDict), library="np", step_size="100 MB") :
+processedEvents = 0
+nextProgressPercentage = 0
+nextProgressTime = startTime
+uprootExecutor=uproot.ThreadPoolExecutor(num_workers=os.cpu_count())
+with uproot.open(rootFilePath, num_workers=os.cpu_count(), decompression_executor=uprootExecutor, interpretation_executor=uprootExecutor) as rootFile :
+  totalEntries = rootFile['event0'].num_entries
+  print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), f"- {totalEntries:.2e} events to process.")
+  for moduleDictBlock in rootFile['event0'].iterate(tuple(mvmeModulesChannelsDict), library="np", decompression_executor=uprootExecutor, interpretation_executor=uprootExecutor) :
     for moduleName, moduleData in moduleDictBlock.items() :
-      for channelNo in mvmeModulesChannelsDict[moduleName] :
-        detData=moduleData[:,channelNo]
-        detData=detData[~np.isnan(detData)]
-        processDataBlock((rootFilePath, moduleName, f"Det_{channelNo+1}"), detData)
+      if exportRaw or exportCal:
+        for channelNo in mvmeModulesChannelsDict[moduleName] :
+          detData = moduleData[:,channelNo]
+          detData = detData[~np.isnan(detData)]
+          processDataBlock((rootFilePath, moduleName, f"Det_{channelNo+1}"), detData)
       if exportAddback and inCalFilePath is not None and moduleName in addbackChannelDict :
         for addbackNo, channels in addbackChannelDict[moduleName].items() :
           processAddbackDataBlock((rootFilePath, moduleName, f"Addback_{addbackNo}"), channels, moduleData.T[channels])
-
+    if verbose:
+      processedEvents += moduleData.shape[0]
+      now = datetime.datetime.now()
+      if True or processedEvents/totalEntries >= nextProgressPercentage or now >=  nextProgressTime:
+        nextProgressPercentage = round(processedEvents/totalEntries/0.05) * 0.05 + 0.05
+        nextProgressTime = now + datetime.timedelta(minutes=3)
+        remainingSeconds = int((totalEntries/processedEvents - 1) * (now - startTime).total_seconds())
+        print(now.strftime("%Y-%m-%d %H:%M:%S"), f"- Processed {processedEvents:.2e} events so far (≈{processedEvents/totalEntries:7.2%}) - ETA: {remainingSeconds//(60*60)}:{(remainingSeconds//60)%60:02}:{remainingSeconds%60:02} - Mean processing speed: {processedEvents/(now - startTime).total_seconds():.2e} events/s")
 
 # Export histograms and a hdtv calibration file
 if len(hists) > 0 :
@@ -232,13 +247,13 @@ if len(hists) > 0 :
     for histName, hist in hists.items() :
       if verbose:
         print("Writing",histName,"...")
-      outfilename=os.path.join(outDir, histName)
+      outfilename = os.path.join(outDir, histName)
       np.savetxt(outfilename, hist["cts"], fmt="%d")
       calOutputFile.write(histName + ": " + str(hist["binning"]["binWidth"]/2+hist["binning"]["lowerEdge"]) + "   " + str(hist["binning"]["binWidth"]) + "\n")
 else:
-  print("There were no spectra created! You might want to check your settings...?")
+  print(now.strftime("%Y-%m-%d %H:%M:%S"), "- There were no spectra created! You might want to check your settings...?")
 
-elapsedSeconds=int((datetime.datetime.now()-startTime).total_seconds())
+elapsedSeconds = int((datetime.datetime.now()-startTime).total_seconds())
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),"-", programName, f"has finished, took {elapsedSeconds//(60*60)}:{(elapsedSeconds//60)%60:02}:{elapsedSeconds%60:02}")
 if verbose:
   print(80*"#")
